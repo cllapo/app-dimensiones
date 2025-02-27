9ee8271687aa748b6f749cd8e75342d71d09f3cd
Leonardo Lapo <cllapo@espe.edu.ec>
2025-02-27 11:47:05 -0500

    Ajuste de escala

--- src/App.jsx
+++ src/App.jsx
@@ -135,7 +135,7 @@ function App() {
 
             console.log("Matriz de homografía:", homography.data64F);
 
-            const scale = 3.0;
+            const scale = 8.0;
             const width = Math.abs(homography.data64F[0]) * scale;
             const height = Math.abs(homography.data64F[4]) * scale;
             const depth = Math.abs(homography.data64F[8]) * scale;
